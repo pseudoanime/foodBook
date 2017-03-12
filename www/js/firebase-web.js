a43dca563e5c3939100708c86c6c4257f28dc43e
pseudoanime <luckilaxmi@gmail.com>
2017-03-12 00:04:06 +0000

    instlled ionic

---
+++ www/js/firebase-web.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDX5D171k0lU23SLQHV3wjCx7I7UbC-HX0",
+    authDomain: "testing-app-35739.firebaseapp.com",
+    databaseURL: "https://testing-app-35739.firebaseio.com",
+    storageBucket: "testing-app-35739.appspot.com",
+    messagingSenderId: "558840194970"
+  };
+  firebase.initializeApp(config);
+</script>
